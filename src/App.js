6db76d47886ae01d4abdc5002784db4eb7c2fa03
Home <araa9977@mail.ru>
2025-04-22 09:56:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './components/Header'
+
+
+class App extends React.Component{
+  helpText = "Help text!"
+  render(){
+    return( <div className="name">
+      <Header title="Шапка сайта"/>
+      <h1>{this.helpText}</h1>
+      <input placeholder={this.helpText} 
+             onClick={this.inputClick}
+             onMouseEnter={this.mouseOver}
+            >
+      </input>
+    <p>{this.helpText === "Help text!" ? "Yes": "No"}</p>
+  </div>)
+  }
+
+  inputClick(){console.log("Clicked")}
+  
+  mouseOver(){console.log("Clicked")}
+}
+
+export default App
